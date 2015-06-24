b2498451945b1897591e731e73e498cfa803d6ad
wannabeCitizen <doorsofskirpan@gmail.com>
2015-06-24 17:13:01 -0400

    added align

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #Run this after cloning dotfiles into your repo
-mv .vimrc ~/.vimrc
+mv vimrc ~/.vimrc
 
 mkdir ~/.vim && mkdir ~/.vim/bundle && mkdir ~/.vim/autoload
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
@@ -8,6 +8,6 @@ cd ~/.vim/bundle
 git clone https://github.com/altercation/vim-colors-solarized.git
 git clone https://github.com/scrooloose/nerdtree.git
 git clone --recursive https://github.com/davidhalter/jedi-vim.git
-
+git clone https://github.com/vim-scripts/Align.git
 echo "All done setting up your dotfiles!"
 exit 0
