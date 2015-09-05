35b3f046f96af9ae7165bc836d1d8838fe1ad737
wannabeCitizen <doorsofskirpan@gmail.com>
2015-09-05 15:03:36 -0600

    improved shell prompt

--- setup.sh
+++ setup.sh
@@ -14,7 +14,7 @@ USER=`id -nu`
 BASHRC=".bashrc"
 if [-e "/home/$USER/$BASHRC"]
 then    
-    echo "export PS1='`if [ $? = 0 ]; then echo "\[\033[01;32m\]✔"; else echo "\[\033[01;31m\]✘"; fi` \[\033[01;30m\]\h\[\033[01;34m\] \w\[\033[0;36m\]\$(__git_ps1 " %s") \[\033[01;30m\]>\[\033[00m\] '" >> /home/$USER/$BASHRC
+    echo "export PS1='`if [ $? = 0 ]; then echo "\[\033[01;32m\]✔"; else echo "\[\033[01;31m\]✘"; fi` \[\033[01;30m\]\h\[\033[01;34m\] \w\[\033[0;36m\] \$(__git_ps1 " %s") \[\033[01;30m\]>\[\033[00m\] '" >> /home/$USER/$BASHRC
 fi
 
 echo "All done setting up your dotfiles!"
